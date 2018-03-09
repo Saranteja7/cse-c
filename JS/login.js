07c8fdea24e0557e06ae40487211261a776cb11f
Saran Teja <37169874+Saranteja7@users.noreply.github.com>
2018-03-09 23:04:28 +0530

    Add files via upload

---
+++ JS/login.js
@@ -0,0 +1,16 @@
+
+function validate(){
+var username = document.getElementById("username").value;
+if ( username == "315177110122" || username == "315177110123" || username == "315177110124" || username == "315177110125" || username == "315177110126" || username == "315177110127" || username == "315177110128" || username == "315177110129" || username == "315177110130"||username == "315177110131" ||username == "315177110132" || username == "315177110133" || username == "315177110134" || username == "315177110135" || username == "315177110136" || username == "315177110137" || username == "315177110138" || username == "315177110139" || username == "315177110140"||username == "315177110141" ||username == "315177110142" || username == "315177110143" || username == "315177110144" || username == "315177110145" || username == "315177110146" || username == "315177110147" || username == "315177110148" || username == "315177110149" || username == "315177110150"||username == "315177110151" ||username == "315177110152" || username == "315177110153" || username == "315177110154" || username == "315177110155" || username == "315177110156" || username == "315177110157" || username == "315177110158" || username == "315177110159" || username == "315177110160"||username == "315177110161" ||username == "315177110162" || username == "315177110163" || username == "315177110164" || username == "315177110165" || username == "315177110166" || username == "315177110167" || username == "315177110168" || username == "315177110169" || username == "315177110170"||username == "315177110171" ||username == "315177110172" || username == "315177110173" || username == "315177110174" || username == "315177110175" || username == "315177110176" || username == "315177110177" || username == "315177110178" || username == "315177110179" || username == "315177110180"||username == "315177110181" ||username == "315177110182" || username == "315177110183" || username == "315177110184" || username == "315177110185" || username == "315177110186" || username == "315177110187" || username == "315177110188" || username == "315177110189" || username == "315177110190")
+{
+alert ("Welcome...!!");
+window.location = "1.html"; // Redirecting to other page.
+return false;
+}
+else
+{
+	alert("Invalid Regd.Number.");
+	window.location= "index.html";
+	return false;
+}
+}
